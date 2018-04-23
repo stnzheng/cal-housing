fa131c2917025baa73504e6a4876c630b2b1239a
Steven Zheng <stnzheng@gmail.com>
2018-04-22 19:22:31 -0700

    added devise route and view split for users/subletters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :subletters, path: 'subletter'
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, path: 'user'
   resources :users
 end
