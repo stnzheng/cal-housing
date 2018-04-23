36d82f5ce24ee092516e18debc90f98af1b32cb7
Nalin Chopra <nalin.chopra@berkeley.edu>
2018-04-22 19:22:25 -0700

    cleaned up schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180423015956) do
 
-  create_table "landlords", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.integer "listingID"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "listings", force: :cascade do |t|
     t.string "author"
     t.integer "price"
@@ -31,15 +23,6 @@ ActiveRecord::Schema.define(version: 20180423015956) do
     t.date "end_date"
     t.integer "rating"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "subleasers", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.integer "listingID"
-    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
