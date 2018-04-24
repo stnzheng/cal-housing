215cfcaeba6a448238cb384b6414a9f535fba5e8
Vanessa Cortes <vcortes@berkeley.edu>
2018-04-24 16:16:55 -0700

    added berkeley.edu email authentication for subleasers

--- app/models/subletter.rb
+++ app/models/subletter.rb
@@ -4,5 +4,6 @@ class Subletter < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  
+  validates_format_of :email, with: /\@berkeley\.edu/, message: 'Make sure to sign up with your berkeley.edu'
+
 end
